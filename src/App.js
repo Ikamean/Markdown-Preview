3f3ac0f7976340ef8650a966e0406863f2f7ba22
unknown <ika.bidzishvili@gmail.com>
2020-12-04 19:37:03 +0400

    finished markdown previewer project with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
+import "./App.scss"
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function App() {
+
+
+
+
+export default function App () {
+  const defaultInput = `
+  # Hello World
+
+  ## Hello World
+  
+  \`<div></div>\`
+  
+  \`\`\`
+  
+  function helloWorld(hello, world) {
+    if (hello == '\`\`\`' && world == '\`\`\`') {
+      return HelloWorld;
+    }
+  }
+  \`\`\`
+  
+ **bold**
+  [CodingIsFun](https://www.freecodecamp.com), 
+  > FreeCodeCamp!
+  
+  - Coding
+    - Is
+       - Fun
+          - FreeCodeCamp.com
+  
+  
+  1. First
+  1. Second
+  1. Third
+  
+  ![FreeCodeCamp Logo w/ Text](https://popsql.com/static/external-logos/freecodecamp.png)
+  `;
+
+
+  const [input, setInput] = useState(defaultInput);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1 id="markdown-prev">Markdown Preview</h1>
+        <div className="editor">
+            <p id="typehere">Type Here</p>
+            <textarea id="editor" value={input} onChange={
+              e =>  setInput(e.target.value)
+            }/>
+        </div>
+        <div className="preview">
+            <p id="prev">Preview</p>
+            <div id="preview" >
+                <ReactMarkdown source={input} renderers =  {{ code:Component }}   />
+          </div>
+        </div>
+      </div>
   );
 }
 
-export default App;
+
+
+const Component = ({value,language}) => {
+  
+  return (
+    <SyntaxHighlighter language={language ?? null} style={dark}>
+      {value ?? ''}
+    </SyntaxHighlighter>
+  );
+};
+
+
